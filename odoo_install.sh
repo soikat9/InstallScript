9508fda671f3ee38ba3e50f1387eb4dc8c190d50
soikat9 <32137011+soikat9@users.noreply.github.com>
2024-06-13 01:44:23 +0600

    Update odoo_install.sh UID & GID = 1000

--- odoo_install.sh
+++ odoo_install.sh
@@ -136,9 +136,11 @@ else
 fi
 
 echo -e "\n---- Create ODOO system user ----"
-sudo adduser --system --quiet --shell=/bin/bash --home=$OE_HOME --gecos 'ODOO' --group $OE_USER
+sudo addgroup --gid 1000 $OE_USER
+sudo adduser --system --quiet --shell=/bin/bash --home=$OE_HOME --gecos 'ODOO' --uid 1000 --gid 1000 $OE_USER
 #The user should also be added to the sudo'ers group.
 sudo adduser $OE_USER sudo
+
 
 echo -e "\n---- Create Log directory ----"
 sudo mkdir /var/log/$OE_USER
